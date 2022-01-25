cf6ed62c751754e5cc61a5bd6379944ba7037bec
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-01-25 12:48:02 +0300

    Platformer project initialization

---
+++ Platformer/Source/Platformer/PlatformerGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlatformerGameMode.h"
+#include "PlatformerCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APlatformerGameMode::APlatformerGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
