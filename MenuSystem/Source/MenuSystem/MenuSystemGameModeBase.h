b10458807e4c67ef524e6e7fbe5b47db2617894f
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-01-27 13:59:14 +0300

    Menu System project with classes to implement host/join in GUI

---
+++ MenuSystem/Source/MenuSystem/MenuSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MenuSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MENUSYSTEM_API AMenuSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
